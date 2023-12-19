009dec284f7b1cdb03cf7194924087d04f35dbc9
Haruki <nmd03pvt@gmail.com>
2023-12-19 13:46:34 +0700

    refactor:  > move my first program sample into  folder

--- LED-program.ino
+++ LED-program.ino
@@ -39,20 +39,21 @@ void setup()
 // things to do on looping
 void loop()
 {
+  turnOn();
   // flicker mode
-  flicker();
+  // flicker();
 
-  // light each layer up and down on order
-  layerUpDown();
+  // // light each layer up and down on order
+  // layerUpDown();
 
-  // light each layer up and down but faster
-  layerUpDownFaster();
+  // // light each layer up and down but faster
+  // layerUpDownFaster();
 
-  // raining effect
-  randRain();
+  // // raining effect
+  // randRain();
 
-  // spiral effect
-  spiralInAndOut();
+  // // spiral effect
+  // spiralInAndOut();
 
   // turnOnAndOffAllByColumnSideways();
 }
