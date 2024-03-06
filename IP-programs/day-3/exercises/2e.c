e86bb01e17a5e945ebc1229982b85f7600c7f245
Haruki Nguyen <nmd03pvt@gmail.com>
2024-03-06 15:38:53 +0700

    feat: `exercises/2e.c` > add new exercise

---
+++ IP-programs/day-3/exercises/2e.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if (n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+int main() {
+  int n;
+
+  printf("Nhap n: ");
+  scanf("%d", &n);
+
+  if (n == 1) {
+    printf("Giai thua cua 1 la 1");
+  } else {
+    int i;
+    unsigned long long sum = 0;
+    for (i = 1; i <= n; i++) {
+      printf("Giai thua la: %d\n", factorial(i));
+      sum += factorial(i);
+    }
+    printf("Tong giai thua tu 1 toi %d la: %llu", n, sum);
+  }
+
+  return 0;
+}
