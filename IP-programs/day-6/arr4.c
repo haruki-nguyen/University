4c83b53ce85f5c63e66ee96797cff9a3686723b7
Haruki Nguyen <nmd03pvt@gmail.com>
2024-03-21 20:47:42 +0700

    feat: `day-6/arr4.c` > update code

--- IP-programs/day-6/arr4.c
+++ IP-programs/day-6/arr4.c
@@ -30,6 +30,28 @@ int sumChars(int a[], int *n, int (*check)(int n)) {
   return s;
 }
 
+int uniqeCount(int a[], int *n) {
+  int i, j, c = 0;
+  int isUnique[*n];
+
+  for (i = 0; i < *n; i++) {
+    isUnique[i] = 1;
+  }
+
+  for (i = 0; i < *n; i++) {
+    if (isUnique[i]) {
+      c++;
+      for (j = i + 1; j < *n; j++) {
+        if (a[i] == a[j]) {
+          isUnique[j] = 0;
+        }
+      }
+    }
+  }
+
+  return c;
+}
+
 int main() {
 
   int n;
@@ -44,6 +66,7 @@ int main() {
 
   printf("Tong phan tu co chu so dau tien le la: %d\n",
          sumChars(a, &n, isFirstCharOdd));
-  printf("Tong phan tu la so chinh phuong la: %d",
+  printf("Tong phan tu la so chinh phuong la: %d\n",
          sumChars(a, &n, isSquareNumber));
+  printf("So phan tu phan biet la: %d", uniqeCount(a, &n));
 }
