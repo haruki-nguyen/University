222f22a7c87782eff3a2767d3e4fd2b721c84429
Haruki Nguyen <nmd03pvt@gmail.com>
2024-11-06 20:35:37 +0700

    doc: `secret-number.py` > add document comment

--- Python-traning/test-1/secret-number.py
+++ Python-traning/test-1/secret-number.py
@@ -1,6 +1,8 @@
 import math
 
+# WRONG
 # Document to find n and k: "./secret-number-doc.md"
+# or "https://github.com/haruki-nguyen/University/blob/main/Python-traning/test-1/secret-number-doc.md"
 
 sample_x1 = 2
 sample_x2 = 3
@@ -12,6 +14,6 @@ a = int(input())
 n = (math.log(sample_y1/sample_y2))/(math.log(sample_x1/sample_x2))
 k = (sample_y1 / (sample_x1 ** n))
 
-y = k * (a ** n)
+y = int(round(k * (a ** n)))
 
-print(round(y))
+print(y)
